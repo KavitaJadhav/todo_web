3912959989014ee6be07b5c5344773f577459545
KavitaJadhav <jadhavkavita206@gmail.com>
2023-05-02 11:07:51 +0530

    redirecting user to welcompe page on successfull login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
   displayError : boolean = false
   errorMessage : string = 'Invalid crediantials'
   username : string = ''
   password : string = ''
 
+  constructor(private router : Router){
+    
+  }
+
   loginUser(){
     if(this.username==='abc' && this.password==='password'){
-      console.log('login successfull')
       this.displayError = false
+      this.router.navigate(['welcome', this.username])
     }else{
       this.displayError = true
     }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  username : string = ''
 
+  constructor(private route:ActivatedRoute){
+
+  }
+  ngOnInit(){
+    this.username = this.route.snapshot.params['name']
+  }
 }
