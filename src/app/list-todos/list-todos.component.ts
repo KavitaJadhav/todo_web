913be396c1268614670401b409131e44a856434e
KavitaJadhav <jadhavkavita206@gmail.com>
2023-05-02 18:28:35 +0530

    Added todo class to represent todo.

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 
+export class Todo{
+  constructor(
+      public id : number ,
+      public description:string,
+      public complete: boolean,
+      public targetDate: Date ){
+  
+    }
+  }
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
-export class ListTodosComponent {
 
+export class ListTodosComponent {
+  
   todos = [
-    {id: 1, description: 'task 1'},
-    {id: 2, description: 'task 2'},
-    {id: 3, description: 'task 3'}
+      new Todo(1, 'task1',true, new Date() ),
+      new Todo(2, 'task2',false, new Date() ),
+      new Todo(3, 'task3',false, new Date() ),
+      new Todo(4, 'task4',false, new Date() ),
   ]
 }
