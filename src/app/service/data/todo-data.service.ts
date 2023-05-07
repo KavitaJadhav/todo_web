bc02dd21f0223978f3226e80e0664546a7e19146
KavitaJadhav <jadhavkavita206@gmail.com>
2023-05-07 14:24:43 +0530

    Added basic auth in list todo api call.

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { Todo } from 'src/app/list-todos/list-todos.component';
 
@@ -12,7 +12,8 @@ export class TodoDataService {
   constructor(private http:HttpClient) { }
 
   public retriveAllTodos(username:string){
-    return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`)
+    let headers = new HttpHeaders({Authorization: this.basicAuthorizationString()})
+    return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`, {headers})
   }
 
   deleteTodo(username: string, id: number) {
@@ -29,5 +30,12 @@ export class TodoDataService {
 
   updateTodo(username: string, id: number, todo:Todo) {
     return this.http.put(`http://localhost:8080/users/${username}/todos/${id}`, todo)
+  }
+
+  private basicAuthorizationString(){
+    let username = 'username'
+    let password = 'password'
+    let authorizationString = 'Basic ' + window.btoa(username + ':' + password)
+    return authorizationString
   }
 }
